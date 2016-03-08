0d2c9d4aaa455c46a889333fa2691ecc4c11f558
Barak Bar Orion <barak.bar@gmail.com>
2016-03-08 21:25:56 +0200

    Add missing files.

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t create-branch .
--- keys/gen.sh
+++ keys/gen.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 openssl genrsa -out server.key 2048
-openssl req -new -x509 -key server.key -out server.pem -days 3650
+openssl req -config openssl.cnf -new -x509 -key server.key -out server.pem -days 3650
+#openssl x509 -req -days 1024 -in server.csr -signkey server.key -out server.crt
+
+#openssl req -config openssl.cnf -new -key server.key -out server.csr

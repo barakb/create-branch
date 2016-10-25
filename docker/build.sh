307335c7f3483b2d3107ad121c80d2aec6506d97
Barak Bar Orion <barak.bar@gmail.com>
2016-10-25 15:18:12 +0300

    Trigger docker build.

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t barakb/create-branch .
---
+++ docker/serve.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source ~/.bashrc
+
+unset GOPATH
+export GOPATH=/golang/create-branch
+
+
+
+#(cd /golang/create-branch/src/github.com/barakb/create-branch; git fetch; git rebase)
+
+#cd /golang/create-branch
+
+#go build -o bin/create-branch github.com/barakb/create-branch/main
+
+#cd /golang/create-branch/src/github.com/barakb/create-branch
+
+#nvm use v5.7.1
+#npm install
+#webpack
+
+/golang/create-branch/bin/create-branch -repos /create-branch-conf/repos.txt "$*"
